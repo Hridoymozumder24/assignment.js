d23a57b610a9ebe348ec207a351829a9616a1ce8
Hridoymozumder24 <52328719+Hridoymozumder24@users.noreply.github.com>
2021-01-22 18:04:17 +0600

    assignment

---
+++ assignment.js
@@ -0,0 +1,20 @@
+function kilometerToMeter (kilometer)
+{
+         var Distance = kilometer*1000; //1km = 1000m
+          
+          return Distance;
+          
+          
+          
+}
+var meter = kilometerToMeter (5)
+console.log(meter)
+
+
+// // function budgetCalculator (watch,phone,laptop)
+// // {
+          
+          
+          
+          
+// }
