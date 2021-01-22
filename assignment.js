bfc7954c42da31e10b65199552a0ea9cf18e1477
Hridoymozumder24 <52328719+Hridoymozumder24@users.noreply.github.com>
2021-01-22 23:24:09 +0600

    mass

--- assignment.js
+++ assignment.js
@@ -2,66 +2,66 @@
  //
                                                 
                                                 
-          //                                       //01-kilometerToMeter//
+                                                //kilometerToMeter//
  
                   
-          //         function kilometerToMeter (kilometer)
-          //         {
-          //                 var Distance = kilometer*1000; //1km = 1000m
-          //                  return Distance;
+                  function kilometerToMeter (kilometer)
+                  {
+                          var Distance = kilometer*1000; //1km = 1000m
+                           return Distance;
                             
                             
-          //                   }
-          //         var meter = kilometerToMeter (5)
-          //         console.log(meter)
+                            }
+                  var meter = kilometerToMeter (5)
+                  console.log(meter)
                                                       
                   
                   
-          //                                   //02-budgetCalculator//
+                                            //budgetCalculator//
 
-          //         function budgetCalculator (watch,mobile,laptop)
-          //         {
+                  function budgetCalculator (watch,mobile,laptop)
+                  {
                             
-          //                   var total = watch*50+mobile*100+laptop*300;
-          //                   return total;
+                            var total = watch*50+mobile*100+laptop*300;
+                            return total;
                             
                             
                             
-          //           }
-          //         var result = budgetCalculator(2,1,1);
-          //         console.log(result);
+                    }
+                  var result = budgetCalculator(2,1,1);
+                  console.log(result);
  
-          //                            //03-hotelCost//
+                                     //hotelCost//
           
-          // function hotelCost (duration){
-          //           var livingCost =0;
-          //           if(duration <= 10){
-          //           livingCost = duration*100; //first 10 day =100tk;
-          //           }
-          //           else if(duration <= 20){
-          //                     var firstday =10*100;
-          //                     var remaining = duration - 10;
-          //                     var secendDay = remaining * 80;   //  next 10days cost = 80taka
-          //                     livingCost =firstday+secendDay;
-          //           }
-          //           else{
-          //                     var firstday =10*100;
-          //                     var secendDay =10*80;
-          //                     var remaining =duration -20;
-          //                     var thirdDay =remaining*50 // 21 tarikh to 31 tarikh 50 taka
-          //                     livingCost=firstday+secendDay+thirdDay;
+          function hotelCost (duration){
+                    var livingCost =0;
+                    if(duration <= 10){
+                    livingCost = duration*100; //first 10 day =100tk;
+                    }
+                    else if(duration <= 20){
+                              var firstday =10*100;
+                              var remaining = duration - 10;
+                              var secendDay = remaining * 80;   //  next 10days cost = 80taka
+                              livingCost =firstday+secendDay;
+                    }
+                    else{
+                              var firstday =10*100;
+                              var secendDay =10*80;
+                              var remaining =duration -20;
+                              var thirdDay =remaining*50 // 21 tarikh to 31 tarikh 50 taka
+                              livingCost=firstday+secendDay+thirdDay;
                              
                               
-          //           }
-          //           return livingCost;
+                    }
+                    return livingCost;
                     
-          // }
-          //          var totalcost = hotelCost(38);
-          //           console.log(totalcost);
+          }
+                   var totalcost = hotelCost(38);
+                    console.log(totalcost);
                     
                     
                     
-                                      //04-megafriend//
+                                               //04-megafriend
 
     function megaFriend(friendsName) {
       var longestName = '';
@@ -76,13 +76,11 @@
   // Friends Name
 
   var getMegaFriend = megaFriend([
-      'Redoan Arafat',
-      'Rocky Ahmed shuvo',
-      'Hridoy Ahmed',
+      'Hridoy Arafat',
+      'komol Ahmed shuvo',
+      'Tamim Ahmed',
   ]);
   
   // Console Output
-  
-  var getMegaFriend =megaFriend;
+
   console.log(getMegaFriend);
-  
